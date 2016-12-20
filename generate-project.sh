34ae3446263315e35e45fa3c4b4df9f94922dde5
haililihai <haililihai@gmail.com>
2016-12-21 01:07:02 +0800

    ATPP GUI V2.0

--- generate-project.sh
+++ generate-project.sh
@@ -14,42 +14,42 @@ cd "$folder"
 
 
 
-echo "#!/bin/bash" > myproject
-echo ". project.inc" >> myproject
-echo ". widgets.inc" >> myproject
-echo ". functions.inc" >> myproject
-echo ". events.inc" >> myproject
-echo "gtk \"gtk_server_exit\"" >> myproject
-chmod 755 myproject
+echo "#!/bin/bash" > ATPP
+echo ". project" >> ATPP
+echo ". widgets" >> ATPP
+echo ". functions" >> ATPP
+echo ". events" >> ATPP
+echo "gtk \"gtk_server_exit\"" >> ATPP
+chmod 755 ATPP
 
 
-echo "function gtk() { GTK=\`gtk-server msg=\$\$,\"\$@\"\`; }" > project.inc
-echo "function define() { \$2 \"\$3\"; eval \$1=\$GTK; }" >> project.inc
-echo "gtk-server -cfg=gtk-server-glade.cfg -ipc=\$\$ &" >> project.inc
-echo "sleep 0.1" >> project.inc
-echo "gtk \"gtk_init NULL NULL\"" >> project.inc
-echo "gtk \"glade_init\"" >> project.inc
-echo "define XML gtk \"glade_xml_new project.glade 0 0\"" >> project.inc
-echo "gtk \"glade_xml_signal_autoconnect, \$XML\"" >> project.inc
-echo "define window1 gtk \"glade_xml_get_widget \$XML window1\"" >> project.inc
-echo "gtk \"gtk_server_connect \$GTK delete-event window1\"" >> project.inc
-echo "gtk \"gtk_widget_show \$window1\" 1" >> project.inc
-echo "" >> project.inc
+echo "function gtk() { GTK=\`gtk-server msg=\$\$,\"\$@\"\`; }" > project
+echo "function define() { \$2 \"\$3\"; eval \$1=\$GTK; }" >> project
+echo "gtk-server -cfg=gtk-server-glade.cfg -ipc=\$\$ &" >> project
+echo "sleep 0.1" >> project
+echo "gtk \"gtk_init NULL NULL\"" >> project
+echo "gtk \"glade_init\"" >> project
+echo "define XML gtk \"glade_xml_new ATPP.glade 0 0\"" >> project
+echo "gtk \"glade_xml_signal_autoconnect, \$XML\"" >> project
+echo "define window1 gtk \"glade_xml_get_widget \$XML window1\"" >> project
+echo "gtk \"gtk_server_connect \$GTK delete-event window1\"" >> project
+echo "gtk \"gtk_widget_show \$window1\" 1" >> project
+echo "" >> project
 
-echo "" > widgets.inc
-
-
-echo "until [[ \"\$E\" -eq \"window1\" ]];do" > events.inc
-
-#echo "echo \"Event: \$E\"" >> events.inc
-
-echo "    define E gtk \"gtk_server_callback wait\"" >> events.inc
+echo "" > widgets
 
 
-touch functions.inc 2>/dev/null
+echo "until [[ \"\$E\" -eq \"window1\" ]];do" > events
+
+#echo "echo \"Event: \$E\"" >> events
+
+echo "    define E gtk \"gtk_server_callback wait\"" >> events
+
+
+touch functions 2>/dev/null
 
 name=""
-cat project.glade | while read a;do
+cat ATPP.glade | while read a;do
 
 
 	check=`echo "$a" | grep ' id='`
@@ -57,7 +57,7 @@ cat project.glade | while read a;do
 		name=`echo "$check" | sed -e "s/.* id=\"//" -e "s/\".*//"`
 		echo $name
 
-		echo define $name gtk \"glade_xml_get_widget \$XML $name\" >> widgets.inc
+		echo define $name gtk \"glade_xml_get_widget \$XML $name\" >> widgets
 
 	fi
 	check=`echo "$a" |grep ' handler='`
@@ -67,24 +67,24 @@ cat project.glade | while read a;do
 		event=`echo "$check" | sed -e "s/.* name=\"//" -e "s/\".*//"`
 		echo $event
 
-		echo define $name gtk \"glade_xml_get_widget \$XML $name\" >> widgets.inc
+		echo define $name gtk \"glade_xml_get_widget \$XML $name\" >> widgets
 
 		after=`echo "$a" |grep ' after=\"yes'`
 		if [ "$after" != "" ];then
-			echo gtk \"gtk_server_connect_after \$GTK $event ${name}_${event}\" >> widgets.inc
+			echo gtk \"gtk_server_connect_after \$GTK $event ${name}_${event}\" >> widgets
 		else
 
-			echo gtk \"gtk_server_connect \$GTK $event ${name}_${event}\" >> widgets.inc
+			echo gtk \"gtk_server_connect \$GTK $event ${name}_${event}\" >> widgets
 		fi
-		echo "	[[ \"\$E\" == \"${name}_${event}\" ]] && $handler" >> events.inc
-		fcheck=`grep "$handler () {" functions.inc`
-		if [ "$fcheck" == "" ];then
-			echo "$handler () {" >> functions.inc
-			echo "	echo $handler" >> functions.inc
-			echo "}" >> functions.inc
-		fi
+		echo "	[[ \"\$E\" == \"${name}_${event}\" ]] && $handler" >> events
+		fcheck=`grep "$handler () {" functions`
+		#if [ "$fcheck" == "" ];then
+		#	echo "$handler () {" >> functions
+		#	echo "	echo $handler" >> functions
+		#	echo "}" >> functions
+		#fi
 	fi
 done
 
-echo "done" >> events.inc
+echo "done" >> events
 
